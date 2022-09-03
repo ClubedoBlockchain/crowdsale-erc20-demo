0408f09577a9a367e5ab0b1e9102a5d36fa7afd6
Dion Maicon E. Duarte <dionmaicon@outlook.com>
2022-09-03 13:56:39 +0100

    feat: rust deploy

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -68,6 +68,7 @@ module.exports = {
       network_id: 5,       // Goerli's network id
       chain_id: 5,         // Goerli's chain id
       gas: 5500000,        // Gas limit used for deploys.
+      websockets: true,
       confirmations: 0,    // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets)networkCheckTimeout
