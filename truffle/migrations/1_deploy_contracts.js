f3e9c5e1f28bf79903fb29e90786525a9ad7f939
Dion Maicon E. Duarte <dionmaicon@outlook.com>
2022-09-04 12:37:57 +0100

    fix: too many requests

--- truffle/migrations/1_deploy_contracts.js
+++ truffle/migrations/1_deploy_contracts.js
@@ -7,13 +7,32 @@ require('dotenv').config();
 const TOTAL_SUPPLY = process.env.TOTAL_SUPPLY;
 const RATE = process.env.RATE;
 
+const delay = async (timeout) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('delayed:', timeout);
+      resolve("Clear")
+    }, timeout)
+  })
+}
+
 module.exports = async (deployer) => {
   let accounts = await web3.eth.getAccounts();
   
+  
   await deployer.deploy(BLKToken, TOTAL_SUPPLY);
+  await delay(4000);
+
   await deployer.deploy(BLKTokenKYC);
+
+  await delay(4000);
+
   await deployer.deploy(BLKTokenSale, web3.utils.toWei(RATE, "wei"), accounts[0], BLKToken.address, BLKTokenKYC.address);
   
+  await delay(4000);
+
   let instance = await BLKToken.deployed();
+
+  await delay(4000);
   await instance.transfer(BLKTokenSale.address, TOTAL_SUPPLY);
 };
