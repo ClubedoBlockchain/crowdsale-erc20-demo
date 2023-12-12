df95c9fbc34ea8259736c9e9f1da25c6b22a5e99
Dion Maicon <dionmaicon@outlook.com>
2023-12-11 23:50:27 -0300

    fix: crypto lib

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -5,5 +5,7 @@ module.exports = function override(webpackConfig) {
         type: "javascript/auto"
     });
 
+    webpackConfig.resolve = { ...webpackConfig.resolve, ...{ fallback: { crypto: require.resolve("crypto-browserify") } } } 
+
     return webpackConfig;
 }
